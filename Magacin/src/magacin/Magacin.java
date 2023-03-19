c9d0635776e14cc6df21ebccdf8e3b7af5da6051
Stojan123 <milosevicdejan65@gmail.com>
2023-03-19 18:21:35 +0100

    Dopunjena implementacija metoda interfejsa MagacinInterfejs u okviru main grane

--- Magacin/src/magacin/Magacin.java
+++ Magacin/src/magacin/Magacin.java
@@ -1,24 +1,55 @@
 package magacin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import interfejs_magacin.MagacinInterfejs;
 
-public class Magacin implements MagacinInterfejs{
+public class Magacin implements MagacinInterfejs {
+
+	List<Artikal> artikli = new ArrayList<>();
 
 	@Override
 	public void dodajArtikal(Artikal a) {
-		// TODO Auto-generated method stub
-		
+		if (a == null) {
+			return;
+		}
+
+		if (!artikli.contains(a)) {
+			artikli.add(a);
+			a.setKolicina(1);
+		} else {
+			a.setKolicina(a.getKolicina() + 1);
+		}
+
 	}
 
 	@Override
 	public void izbaciArtikal(Artikal a) {
-		// TODO Auto-generated method stub
-		
+		if (a == null) {
+			return;
+		}
+		if (!artikli.contains(a)) {
+			return;
+		}
+		if (a.getKolicina() == 0) {
+			return;
+		}
+
+		a.setKolicina(a.getKolicina() - 1);
 	}
 
 	@Override
 	public Artikal pronadjiArtikal(int sifra) {
-		// TODO Auto-generated method stub
+		if (sifra < 0) {
+			return null;
+		}
+
+		for (Artikal ar : artikli) {
+			if (ar.getSifra() == sifra) {
+				return ar;
+			}
+		}
 		return null;
 	}
 

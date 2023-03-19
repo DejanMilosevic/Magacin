257a475a6841f27ade8d5c3f09973da757248b9d
Stojan123 <milosevicdejan65@gmail.com>
2023-03-19 17:44:55 +0100

    Napravljena klasa Magacin koja implementira interfejs MagacinInterfejs u main grani

---
+++ Magacin/src/magacin/Magacin.java
@@ -0,0 +1,25 @@
+package magacin;
+
+import interfejs_magacin.MagacinInterfejs;
+
+public class Magacin implements MagacinInterfejs{
+
+	@Override
+	public void dodajArtikal(Artikal a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Artikal pronadjiArtikal(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
